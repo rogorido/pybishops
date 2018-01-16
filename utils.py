cfefec56f94b87f85cf2b284465aa145fc7aaf6f
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-16 17:59:30 +0100

    Más órdenes religiosas.

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@
 # en el que uso break con el primero que encuentra
 ordenes = ('O.P.', 'O.S.M.', 'C.R.S.A.', 'C.R.M.', 'O.F.M. Obs.',
            'O.F.M. Conv.', 'O.F.M. Cap.', 'O.F.M.', 'O.S.B', 'O.M.', 'O.Cist.',
-           'O.Praem.', 'C.R.L.', 'C.S.Ch.',
+           'O.Praem.', 'C.R.L.', 'C.S.Ch.', 'O.S.H.', 'T.O.R.',
            'O.S.Io.Hieros.', 'O.Ss.C.A.', 'S.J', 'O.S.',
            'C.R.')
 
