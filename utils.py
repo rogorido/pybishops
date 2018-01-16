a78535799a6b9e18a7c6e1bb8371724d2731ad61
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-16 16:57:40 +0100

    Añadidos cargos.

--- utils.py
+++ utils.py
@@ -4,7 +4,13 @@
 # ponemos lo de Obs. antes de OFM porque tengo un for
 # en el que uso break con el primero que encuentra
 ordenes = ('O.P.', 'O.F.M. Obs.', 'O.F.M.', 'O.S.B', 'O.M.',
-           'O. Praem.', 'O.S.Io.Hieros.', 'O.Ss.C.A.')
+           'O. Praem.', 'O.S.Io.Hieros.', 'O.Ss.C.A.',
+           'S.J', 'O.S.')
 
 meses = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
          'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+motivos = ('Died', 'Resigned', 'Appointed', 'Retired', 'Succeeded',
+           'Confirmed')
+
+cargos = ('Archbishop', 'Administrator', 'Bishop')
