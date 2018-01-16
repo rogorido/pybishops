26df7f121b153134aed5782dd6869ed30ab2440e
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-16 17:20:54 +0100

    Bregar con paréntesis en nombres, etc. [cutre!]

--- obispo.py
+++ obispo.py
@@ -12,6 +12,7 @@ class Obispo:
         """El string creador es lo que viene de BeautifulSoup con
         el formateado de html."""
 
+        self.creador = creador
         # realmente el primer <a> es el nombre, aunque esto así hay
         # que tener cuidado. Lo guardamos como html porque nos permite sacar
         # lo que está en negrita que es el apellido. 
@@ -19,10 +20,19 @@ class Obispo:
         self.nombre = nombrehtml.text
         apellidohtml = nombrehtml.b
         self.apellido = apellidohtml.text
+
+        # metemos el texto inicial en una variable 
+        self.cadena = creador.text
+        # esto es un poco cutre: a veces en el nombre/apellido
+        # hay paréntesis y eso crea un lío cuando luego queremos
+        # extraer los datos de las fechas. Con esto quitamos el nombre
+        # de la cadena esa. Un poco primitivo... habría que hacer un check
+        # de cuántos paréntesis hay, etc. 
+        self.cadena = self.cadena.replace(self.nombre, '')
+        print(self.cadena)
+
         # quitamos el apellido del nombre para tener el nombre final.
         self.nombre = self.nombre.replace(self.apellido, '')
-
-        self.cadena = creador.text
 
         self.__extractOrder()
         self.__extractFechas()
@@ -56,4 +66,13 @@ class Obispo:
         # las dividimos. Es importante usar ' - ' porque a veces
         # hay un guión en esa cadena. Cuidado con esto.
         fecha = f.Fechas(fechas)
-        
+        self.motivofin = fecha.motivofin
+
+        print(self.motivofin, fecha.nombramiento)
+
+        # si fecha.destino es true, quiere decir que el tipo se mueve
+        # y por tanto intentamos extraer el último anchor que suele ser
+        # la referencia a adonde se mueve. 
+        if fecha.destino:
+            anchor = self.creador.find_all('a')[-1]
+            print(anchor['href'])
