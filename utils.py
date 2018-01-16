76e371b1350b1e0d4dbfbb26421367b51c385fbd
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-16 17:21:11 +0100

    Más órdenes religiosas.

--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@
 
 # ponemos lo de Obs. antes de OFM porque tengo un for
 # en el que uso break con el primero que encuentra
-ordenes = ('O.P.', 'O.F.M. Obs.', 'O.F.M.', 'O.S.B', 'O.M.',
-           'O. Praem.', 'O.S.Io.Hieros.', 'O.Ss.C.A.',
-           'S.J', 'O.S.')
+ordenes = ('O.P.', 'O.F.M. Obs.', 'O.F.M. Conv.', 'O.F.M.', 'O.S.B',
+           'O.M.', 'O. Cist.', 'O. Praem.', 'O.S.Io.Hieros.',
+           'O.Ss.C.A.', 'S.J', 'O.S.', 'C.R.')
 
 meses = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
          'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
@@ -13,4 +13,5 @@ meses = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
 motivos = ('Died', 'Resigned', 'Appointed', 'Retired', 'Succeeded',
            'Confirmed')
 
-cargos = ('Archbishop', 'Administrator', 'Bishop')
+cargos = ('Archbishop (Personal Title)', 'Archbishop', 'Administrator',
+          'Bishop')
