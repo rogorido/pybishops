f9518462b7c5620fee9e5d39ddf50120b8cf9e1b
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-16 16:14:42 +0100

    Fichero utils.py con tuples, etc.

---
+++ utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ponemos lo de Obs. antes de OFM porque tengo un for
+# en el que uso break con el primero que encuentra
+ordenes = ('O.P.', 'O.F.M. Obs.', 'O.F.M.', 'O.S.B', 'O.M.',
+           'O. Praem.', 'O.S.Io.Hieros.', 'O.Ss.C.A.')
+
+meses = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+         'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
