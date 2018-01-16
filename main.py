b91e41a1f8e7c090632c592aafdb01c2113f7f27
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-16 16:14:56 +0100

    Clase Fechas para gestionar el lío de fechas.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import diocesis as d
+
+dioc = d.Diocesis('http://www.catholic-hierarchy.org/diocese/drace.html',
+                  'd3')
+
+#dioc = d.Diocesis('http://www.catholic-hierarchy.org/diocese/dr506.html', 'd3')
+
+bishops = dioc.getObispos()
+
+print('\nEstas son las órdenes\n---------------\n')
+for o in bishops:
+    if o.getOrder() is not None:
+        print(o.getName())
+        print(o.getOrder())
+
+
