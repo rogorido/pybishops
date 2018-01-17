c493ef6a8ecdb405bf5f110b940b7eec79e525d7
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-17 14:04:34 +0100

    Uso de argparse para las opciones de consola.

--- main.py
+++ main.py
@@ -2,18 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import diocesis as d
+import psycopg2
+import sys
+import argparse
 
-dioc = d.Diocesis('http://www.catholic-hierarchy.org/diocese/drace.html',
+# Analizamos si se ha pasado la opción -s que sirve
+# solamente para simular sin más, sin meter los datos en la bd.
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--simular", help="Simular sin meter los datos",
+                    action="store_false")
+args = parser.parse_args()
+if args.simular:
+    print("Ejecutando con normalidad: metiendo en la bd")
+    try:
+        conn = psycopg2.connect("dbname='dominicos' user='igor' host='localhost'")
+        cur = conn.cursor()
+    except:
+        print("I am unable to connect to the database")
+else:
+    print("Ejecutando en modo simulación")
+
+print('fin')
+
+def introducirDatos(obispo):
+    """Esta es la función general que introduce los datos en la bd.
+    Se le pasa un objeto Obispo."""
+
+    
+
+
+dioc = d.Diocesis('http://www.catholic-hierarchy.org/diocese/dzamb.html',
                   'd3')
-
-#dioc = d.Diocesis('http://www.catholic-hierarchy.org/diocese/dr506.html', 'd3')
 
 bishops = dioc.getObispos()
 
 print('\nEstas son las órdenes\n---------------\n')
 for o in bishops:
-    if o.getOrder() is not None:
-        print(o.getName())
-        print(o.getOrder())
+    # en algunos casos el obispo es none porque realmente no
+    # hay obispos!
+    if o is not None:
+        print(o.__dict__)
 
-
+    
+if args.simular:
+    cur.close()
+    conn.close()
