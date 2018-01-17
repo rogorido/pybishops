895c98c260308cb63fcf1f6402e5c5b75f9e47d3
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-17 16:06:24 +0100

    Función para extraer acrónimos, etc.

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import psycopg2
+
 # ponemos lo de Obs. antes de OFM porque tengo un for
 # en el que uso break con el primero que encuentra
 ordenes = ('O.P.', 'O.S.M.', 'C.R.S.A.', 'C.R.M.', 'O.F.M. Obs.',
@@ -16,5 +18,27 @@ motivos = ('Died', 'Resigned', 'Appointed', 'Retired', 'Succeeded',
            'Confirmed')
 
 cargos = ('Archbishop (Personal Title)', 'Coadjutor Archbishop',
-          'Archbishop', 'Administrator', 'Auxiliary Bishop', 'Bishop', 'Cardinal-Priest',
+          'Archbishop', 'Apostolic Administrator', 'Administrator',
+          'Auxiliary Bishop', 'Coadjutor Bishop', 'Bishop', 'Cardinal-Priest',
           'Cardinal', 'Prelate')
+
+def listaOrdenes():
+    try:
+        conn = psycopg2.connect("dbname='dominicos' user='igor' host='localhost'")
+        cur = conn.cursor()
+    except:
+        print("I am unable to connect to the database")
+
+    cur.execute("""SELECT order_id, order_acronym FROM religious_orders
+    ORDER BY length(order_acronym) DESC""")
+
+    rows = cur.fetchall()
+
+    acronimos = []
+    diccionario = {}
+
+    for row in rows:
+        acronimos.append(row[1])
+        diccionario[row[1]] = row[0]
+
+    return acronimos, diccionario
