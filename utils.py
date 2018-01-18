ac1bc6471106322e85033ba28895693a40844698
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-01-18 18:31:07 +0100

    Borrando cosas antiguas

--- utils.py
+++ utils.py
@@ -3,20 +3,14 @@
 
 import psycopg2
 
-# ponemos lo de Obs. antes de OFM porque tengo un for
-# en el que uso break con el primero que encuentra
-ordenes = ('O.P.', 'O.S.M.', 'C.R.S.A.', 'C.R.M.', 'O.F.M. Obs.',
-           'O.F.M. Conv.', 'O.F.M. Cap.', 'O.F.M.', 'O.S.B', 'O.M.', 'O.Cist.',
-           'O.Praem.', 'C.R.L.', 'C.S.Ch.', 'O.S.H.', 'T.O.R.',
-           'O.S.Io.Hieros.', 'O.Ss.C.A.', 'S.J', 'O.S.',
-           'C.R.')
-
 meses = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
          'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
 motivos = ('Died', 'Resigned', 'Appointed', 'Retired', 'Succeeded',
            'Confirmed')
 
+# ponemos este orden porque luego uso un for 
+# en el que uso break con el primero que encuentra
 cargos = ('Archbishop (Personal Title)', 'Coadjutor Archbishop',
           'Archbishop', 'Apostolic Administrator', 'Administrator',
           'Auxiliary Bishop', 'Coadjutor Bishop', 'Bishop', 'Cardinal-Priest',
